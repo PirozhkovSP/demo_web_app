32c052576b2aeef97f1e971011df36be7e5cf82c
PirozhkovSP <isuperman98@gmail.com>
2021-08-25 22:12:59 +0300

    initial commit

---
+++ src/main/java/com/demo/ats_network/AtsNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ats_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtsNetworkApplication.class, args);
+    }
+
+}
